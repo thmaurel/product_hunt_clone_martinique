5e79c0de22861fa8cd007c4e9274e41a419c65b5
Thibaud Maurel <maurel.thibaud@free.fr>
2022-01-07 10:14:28 -0400

    test & github setup

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.6'
 
 gem 'faker'
+group :development, :test do
+  gem 'launchy'
+end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.4'
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,20 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to root_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :tagline)
+  end
 end
